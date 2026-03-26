7c54546370806e14312ab8ba57ef0c8e34f09e4e
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-03-25 19:47:21 -0700

    Modulos
    modulo dominio, dtos, persistencia, negocio, presentacion inicializados

---
+++ ProyectoJPA/negocio/src/main/java/com/negocio/Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.negocio;
+
+/**
+ *
+ * @author joser
+ */
+public class Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
