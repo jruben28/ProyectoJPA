729cfdbcb1b428be9672f20b60698cb8d1333d64
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-03-26 04:14:41 -0400

    Implemente en BO el metodo calcularPuntos, y calcularTotalGastado, cree BOException, e hice una clase de prueba provisional PruebaClienteBO

--- ProyectoJPA/negocio/src/main/java/BOs/ClienteBO.java
+++ ProyectoJPA/negocio/src/main/java/BOs/ClienteBO.java
@@ -4,28 +4,59 @@
  */
 package BOs;
 
+import DAOs.ClienteDAO;
+import Entidades.Cliente;
+import Entidades.ClienteFrecuente;
+import Entidades.ClienteGeneral;
+import Entidades.Comanda;
+import java.util.List;
+
 /**
  * Business Object de la entidad Cliente.
+ *
  * @author keppler
  */
 public class ClienteBO {
-//
-//    private ClienteDAO clienteDAO;
-//
-//    public ClienteBO() {
-//        this.ClienteDAO = new ClienteDAO();
-//    }
-//    /**
-//     * Calcula 1 punto por cada 20 pesos gastados
-//     * @param idCliente
-//     * @return 
-//     */
-//     public int calcularPuntos(Long idCliente) {
-//        Double total = calcularTotalGastado(idCliente);
-//        Integer puntos= (int)(total / 20);
-//        return puntos; 
-//    }
-//     
-     
+
+    private ClienteDAO clienteDAO;
+
+    public ClienteBO() {
+        this.clienteDAO = new ClienteDAO();
+    }
+
+    /**
+     * Calcula los puntos generados por las compras del cliente 1 punto por cada
+     * 20 pesos
+     *
+     * @param idCliente
+     * @return puntos
+     */
+    public Integer calcularPuntos(Long idCliente) {
+        Double total = calcularTotalGastado(idCliente);
+        Integer puntos = (int) (total / 20);
+
+        if (puntos < 0) {
+            puntos = 0;
+        }
+        return puntos;
+    }
+
+    /**
+     * Calcula el dinero total gastado por el cliente
+     *
+     * @param idCliente
+     * @return total
+     */
+    public Double calcularTotalGastado(Long idCliente) {
+        List<Comanda> comandas = clienteDAO.buscarComandasPorCliente(idCliente);
+        Double total = 0.0;
+        for (Comanda c : comandas) {
+            total += c.getTotal();
+        }
+        if(total<0){
+            total=0.0;
+        }
+        return total;
+    }
 
 }
---
+++ ProyectoJPA/negocio/src/main/java/excepciones/BOException.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author keppler
+ */
+public class BOException extends RuntimeException{
+
+    public BOException() {
+    super();
+    }
+    
+    public BOException(String mensaje) {
+    super(mensaje);
+    }
+    
+    
+    
+}
