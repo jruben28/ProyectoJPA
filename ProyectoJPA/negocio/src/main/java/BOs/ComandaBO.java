9a3eb1469a007fa723afb1bfd63ffb8166a3e2cb
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-03-27 22:20:42 -0700

    Comanda desde persistencia a negocio

---
+++ ProyectoJPA/negocio/src/main/java/BOs/ComandaBO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BOs;
+
+import com.dtos.ComandaDTO;
+import excepciones.NegocioException;
+
+/**
+ *
+ * @author joser
+ */
+public class ComandaBO implements IComandaBO{
+
+    @Override
+    public ComandaDTO agregarComanda() throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
