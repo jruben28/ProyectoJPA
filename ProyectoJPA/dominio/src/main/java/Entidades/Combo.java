e530b1838f682a8900217741d2192f3c5a753e87
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-04-08 23:22:19 -0400

    Agregue la entidad ComboProducto, y tambien la asocié en el persistence xml.

--- ProyectoJPA/dominio/src/main/java/Entidades/Combo.java
+++ ProyectoJPA/dominio/src/main/java/Entidades/Combo.java
@@ -52,13 +52,11 @@ public class Combo implements Serializable {
         this.porcentajeDescuento=porcentajeDescuento;
         this.activo=true;
     }
+    
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getNombre() {
         return nombre;
