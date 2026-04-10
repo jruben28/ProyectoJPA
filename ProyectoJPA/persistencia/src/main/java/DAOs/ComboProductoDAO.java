4d437c64b8a459b0ad04a2f4f76793354c52e01d
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-04-09 22:42:15 -0400

    Implementé la interfaz IComboProductoDAO y su implementacion ComboProductoDAO, solo la estructura, ambas clases en proceso.

---
+++ ProyectoJPA/persistencia/src/main/java/DAOs/ComboProductoDAO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAOs;
+
+import entidades.ComboProducto;
+import excepciones.PersistenciaException;
+import interfaces.IComboProductoDAO;
+
+/**
+ * Implementación de la interfaz de IComboProductoDAO
+ * @author Adrian Mendoza
+ */
+public class ComboProductoDAO implements IComboProductoDAO{
+
+    @Override
+    public ComboProducto agregar(ComboProducto comboProducto) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
