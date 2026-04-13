8bbd89db838873bcb9c4e4911322d5b30be03314
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-04-13 12:46:15 -0700

    Update Ingrediente.java

--- ProyectoJPA/dominio/src/main/java/Entidades/Ingrediente.java
+++ ProyectoJPA/dominio/src/main/java/Entidades/Ingrediente.java
@@ -1,31 +1,124 @@
-package dominio;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
 
+import enums.UnidadDeMedida;
 import java.io.Serializable;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+/**
+ *
+ * @author joser
+ */
 @Entity
-@Table(name = "ingredientes")
+@Table(name = "Ingredientes")
 public class Ingrediente implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ingrediente")
     private Long id;
-
-    @Column(name = "nombre")
+    
+    @Column(name = "nombre", nullable = false)
     private String nombre;
-
-    @Column(name = "stock")
-    private Double stock; // Solo ocupas esto para tu lógica de Productos
-
-    public Ingrediente() {}
-
-    // Getters y Setters mínimos
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
     
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    @Enumerated(EnumType.STRING)
+    @Column(name = "unidad_de_medida", nullable = false)
+    private UnidadDeMedida unidadDeMedida;
     
-    public Double getStock() { return stock; }
-    public void setStock(Double stock) { this.stock = stock; }
+    @Column(name = "stock", nullable = true)
+    private Double stock;
+    
+    @Column(name = "url_imagen", nullable = true)
+    private String urlImagen;
+
+    public Ingrediente() {
+    }
+
+    public Ingrediente(Long id, String nombre, UnidadDeMedida unidadDeMedida, Double stock, String urlImagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.unidadDeMedida = unidadDeMedida;
+        this.stock = stock;
+        this.urlImagen = urlImagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public UnidadDeMedida getUnidadDeMedida() {
+        return unidadDeMedida;
+    }
+
+    public void setUnidadDeMedida(UnidadDeMedida unidadDeMedida) {
+        this.unidadDeMedida = unidadDeMedida;
+    }
+
+    public Double getStock() {
+        return stock;
+    }
+
+    public void setStock(Double stock) {
+        this.stock = stock;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        this.urlImagen = urlImagen;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ingrediente)) {
+            return false;
+        }
+        Ingrediente other = (Ingrediente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingrediente{" + "id=" + id + ", nombre=" + nombre + ", unidadDeMedida=" + unidadDeMedida + ", stock=" + stock + ", urlImagen=" + urlImagen + '}';
+    }
+
 }
