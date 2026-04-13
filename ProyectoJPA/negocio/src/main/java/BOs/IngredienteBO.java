2fe499020aa405e06448f7e0c5bef93edcbb78be
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-04-13 12:15:47 -0700

    Metodos ingredienteBO

--- ProyectoJPA/negocio/src/main/java/BOs/IngredienteBO.java
+++ ProyectoJPA/negocio/src/main/java/BOs/IngredienteBO.java
@@ -5,10 +5,14 @@
 package BOs;
 
 import DAOs.IngredienteDAO;
+import adaptadores.IngredienteAdapter;
 import com.dtos.IngredienteDTO;
+import entidades.Ingrediente;
 import excepciones.NegocioException;
+import excepciones.PersistenciaException;
 import interfaces.IIngredienteBO;
 import interfaces.IIngredienteDAO;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -28,32 +32,95 @@ public class IngredienteBO implements IIngredienteBO{
     
     @Override
     public IngredienteDTO agregarIngrediente(IngredienteDTO ingredienteDTO) throws NegocioException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            validarDatosIngrediente(ingredienteDTO);
+            
+            Ingrediente entidadIngrediente = IngredienteAdapter.dtoAEntidad(ingredienteDTO);
+            Ingrediente ingredienteRecibido = ingredienteDAO.agregarIngrediente(entidadIngrediente);
+            IngredienteDTO ingredienteRecibidoDTO = IngredienteAdapter.entidadADTO(ingredienteRecibido);
+            
+            return ingredienteRecibidoDTO;
+        }
+        catch(Exception ex){
+            throw new NegocioException("Error al gregar ingrediente: " + ex.getMessage());
+        }
     }
 
     @Override
     public IngredienteDTO actualizarStock(Long idIngrediente, Double stock) throws NegocioException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (idIngrediente == null || idIngrediente <= 0 || stock == null || stock < 0) {
+            throw new NegocioException("Los datos ingresados para actualizar el stock no son validos");
+        }
+
+        try{
+            Ingrediente ingrediente = ingredienteDAO.actualizarStock(idIngrediente, stock);
+            
+            return IngredienteAdapter.entidadADTO(ingrediente);
+            
+        }
+        catch(Exception ex){
+            throw new NegocioException("Error al actualizar stock" + ex.getMessage());
+        }
     }
 
     @Override
     public IngredienteDTO eliminarIngrediente(Long idIngrediente) throws NegocioException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (idIngrediente == null || idIngrediente <= 0) {
+            throw new NegocioException("El id del ingrediente a eliminar no es valido");
+        }
+        try{
+            Ingrediente ingredienteEliminado = ingredienteDAO.eliminarIngrediente(idIngrediente);
+            return IngredienteAdapter.entidadADTO(ingredienteEliminado);
+        }
+        catch(Exception ex){
+            throw new NegocioException("Error al tratar de eliminar el ingrediente con id " + idIngrediente);
+        }
+        
     }
 
     @Override
     public IngredienteDTO obtenerIngredientePorId(Long idIngrediente) throws NegocioException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (idIngrediente == null || idIngrediente <= 0) {
+            throw new NegocioException("El id del ingrediente a obtener no es valido");
+        }
+        
+        try {
+            Ingrediente ingredienteBuscado = ingredienteDAO.obtenerIngredientePorId(idIngrediente);
+
+            return IngredienteAdapter.entidadADTO(ingredienteBuscado);
+        }
+        catch(Exception ex){
+            throw new NegocioException("Error al obtener ingrediente por id: " + ex.getMessage());
+        }
     }
 
     @Override
     public List<IngredienteDTO> obtenerIngredientePorFiltro(String filtro) throws NegocioException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            List<Ingrediente> listaEncontrada = ingredienteDAO.obtenerIngredientePorFiltro(filtro);
+            List<IngredienteDTO> listaRetornada = new ArrayList<>();
+            listaEncontrada.forEach(i -> listaRetornada.add(IngredienteAdapter.entidadADTO(i)));
+            
+            return listaRetornada;
+            
+        }
+        catch(Exception ex){
+            throw new NegocioException("Error al tratar de obtener el ingrediente por el filtro buscado.");
+        }
     }
 
     @Override
     public List<IngredienteDTO> obtenerIngredienteTodos() throws NegocioException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            List<Ingrediente> listaEncontrada = ingredienteDAO.obtenerIngredienteTodos();
+            List<IngredienteDTO> listaRetornada = new ArrayList<>();
+            listaEncontrada.forEach(i -> listaRetornada.add(IngredienteAdapter.entidadADTO(i)));
+            
+            return listaRetornada;
+        }
+        catch(Exception ex){
+            throw new NegocioException("Error al obtener la lista de ingredientes");
+        }
     }
     
     /**
