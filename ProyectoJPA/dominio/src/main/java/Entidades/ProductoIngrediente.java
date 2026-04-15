aede28ea83e44a9fbe56748dd17957582052594e
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-04-15 13:40:39 -0700

    Update ProductoIngrediente.java

--- ProyectoJPA/dominio/src/main/java/Entidades/ProductoIngrediente.java
+++ ProyectoJPA/dominio/src/main/java/Entidades/ProductoIngrediente.java
@@ -72,5 +72,29 @@ public class ProductoIngrediente implements Serializable {
         this.ingrediente = ingrediente;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProductoIngrediente)) {
+            return false;
+        }
+        ProductoIngrediente other = (ProductoIngrediente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.ProductoIngrediente[ id=" + id + " ]";
+    }
     
 }
