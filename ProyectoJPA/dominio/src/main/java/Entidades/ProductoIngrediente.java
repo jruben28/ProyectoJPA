b1bc03aedf52789639bfc9d56a18aebd8c62758d
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-04-15 13:36:40 -0700

    Update ProductoIngrediente.java

--- ProyectoJPA/dominio/src/main/java/Entidades/ProductoIngrediente.java
+++ ProyectoJPA/dominio/src/main/java/Entidades/ProductoIngrediente.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Entidades;
+package entidades;
 
-import dominio.Ingrediente;
 import java.io.Serializable;
 import javax.persistence.*;
 
