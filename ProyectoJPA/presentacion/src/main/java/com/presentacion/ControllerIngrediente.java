c3f44d1cca2740d4dce6f8d7efe8e721409a20f1
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-04-16 02:34:06 -0700

    Llamados a nuevos metodos

--- ProyectoJPA/presentacion/src/main/java/com/presentacion/ControllerIngrediente.java
+++ ProyectoJPA/presentacion/src/main/java/com/presentacion/ControllerIngrediente.java
@@ -6,8 +6,11 @@ package com.presentacion;
 
 import BOs.IngredienteBO;
 import com.dtos.IngredienteDTO;
+import com.dtos.ProductoDTO;
+import com.dtos.ProductoIngredienteRDTO;
 import excepciones.NegocioException;
 import interfaces.IIngredienteBO;
+import java.util.ArrayList;
 import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -26,11 +29,18 @@ public class ControllerIngrediente {
     private Stage primaryStage;
     private IngredientePrincipalFrm principalView;
 
+    /**
+     * Constructor clase de control
+     * @param primaryStage 
+     */
     public ControllerIngrediente(Stage primaryStage) {
         this.ingredienteBO = new IngredienteBO();
         this.primaryStage = primaryStage;
     }
     
+    /**
+     * Muestra el menú principal de ingredientes, temporal en lo que se conecta al menú final
+     */
     public void mostrarPrincipal() {
         principalView = new IngredientePrincipalFrm(this);
         Scene scene = new Scene(principalView, 1200, 800);
@@ -43,6 +53,9 @@ public class ControllerIngrediente {
         primaryStage.show();
     }
     
+    /**
+     * Muestra el registro de un ingrediente nuevo
+     */
     public void mostrarRegistro() {
         AgregarIngredienteFrm registro = new AgregarIngredienteFrm(this);
 
@@ -51,33 +64,49 @@ public class ControllerIngrediente {
         registro.show();
     }
     
+ 
+    /**
+     * Muestra el gestor de stock de ingredientes.
+     * @param principal 
+     */
     public void mostrarGestionStockEnPrincipal(IngredientePrincipalFrm principal) {
         GestionStockFrm gestionStock = new GestionStockFrm(this);
         principal.setView(gestionStock);
     }
+  
     
+    /**
+     * Metodo para persistir un ingrediente
+     * @param ingredienteDTO 
+     */
     public void registrarIngrediente(IngredienteDTO ingredienteDTO){
         ingredienteBO.agregarIngrediente(ingredienteDTO);
     }
     
+//    /**
+//     * Muestra el gestor del stock de ingredientes
+//     */
+//    public void mostrarGestionStock() {
+//        GestionStockFrm gestionStock = new GestionStockFrm(this);
+//        Scene scene = new Scene(gestionStock.getRoot(), 1100, 700);
+//        
+//        java.net.URL cssUrl = getClass().getResource(CSS_PATH);
+//        if (cssUrl != null) {
+//            scene.getStylesheets().add(cssUrl.toExternalForm());
+//        } else {
+//            System.out.println("️ ADVERTENCIA: No se encontró CSS.");
+//        }
+//        
+//        primaryStage.setTitle("Gestión de Stock");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
     
-    public void mostrarGestionStock() {
-        GestionStockFrm gestionStock = new GestionStockFrm(this);
-        Scene scene = new Scene(gestionStock.getRoot(), 1100, 700);
-        
-        java.net.URL cssUrl = getClass().getResource(CSS_PATH);
-        if (cssUrl != null) {
-            scene.getStylesheets().add(cssUrl.toExternalForm());
-        } else {
-            System.out.println("️ ADVERTENCIA: No se encontró CSS.");
-        }
-        
-        primaryStage.setTitle("Gestión de Stock");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
     
-    
+    /**
+     * Metodo que ayuda a persistir los cambios al inventario de ingredientes.
+     * @return 
+     */
     public ObservableList<GestionStockFrm.MovimientoStockFila> cargarMovimientosInventario() {
         ObservableList<GestionStockFrm.MovimientoStockFila> filas = FXCollections.observableArrayList();
         try {
@@ -91,6 +120,11 @@ public class ControllerIngrediente {
         return filas;
     }
     
+    /**
+     * Metodo auxiliar, guarda los cambios hechos al stock
+     * @param rows
+     * @throws NegocioException 
+     */
     public void guardarMovimientos(List<GestionStockFrm.MovimientoStockFila> rows) throws NegocioException {
         boolean huboCambios = false;
         
@@ -110,7 +144,148 @@ public class ControllerIngrediente {
         if (!huboCambios) {
             throw new NegocioException("No se registró ninguna entrada nueva para guardar.");
         }
-        System.out.println("Simulando guardado de movimientos en BD...");
         
+    }
+    
+    /**
+     * Muestra el menú para asignar ingredientes a un producto.
+     * @param principal 
+     */
+    public void mostrarAsignarIngredientesEnPrincipal(IngredientePrincipalFrm principal) {
+        AsignarIngredientesFrm asignarView = new AsignarIngredientesFrm(this);
+        principal.setView(asignarView.getRoot());
+    }
+    
+    /**
+     * Obtiene la lista de los productos disponibles, de momento mockeado al no existir el metodo
+     * @return 
+     */
+    public List<ProductoDTO> obtenerProductosDisponibles() {
+        try {
+            //Mock de productos dado que no existe el metodo en ProductosDAO        
+            List<ProductoDTO> listaProductos = new ArrayList<>();
+            
+            ProductoDTO producto1 = new ProductoDTO();
+            producto1.setId(1L);
+            producto1.setNombre("Ensalada");
+            producto1.setDescripcion("Verduras y topings");
+            producto1.setPrecio(100.00);
+            producto1.setTipo("COMIDA");
+
+            
+//            ProductoDTO producto2 = new ProductoDTO();
+//            producto1.setId(2L);
+//            producto1.setNombre("Hamburgesa");
+//            producto1.setDescripcion("Pan con carne y verduras");
+//            producto1.setPrecio(100.00);
+//            producto1.setTipo("COMIDA");
+//            
+//            ProductoDTO producto3 = new ProductoDTO();
+//            producto1.setId(3L);
+//            producto1.setNombre("Pizza");
+//            producto1.setDescripcion("Masa con salsa de tomate");
+//            producto1.setPrecio(100.00);
+//            producto1.setTipo("COMIDA");
+//            
+//            ProductoDTO producto4 = new ProductoDTO();
+//            producto1.setId(4L);
+//            producto1.setNombre("Malteada");
+//            producto1.setDescripcion("Leche con helado");
+//            producto1.setPrecio(100.00);
+//            producto1.setTipo("BEBIDA");
+//            
+//            ProductoDTO producto5 = new ProductoDTO();
+//            producto1.setId(5L);
+//            producto1.setNombre("Helado");
+//            producto1.setDescripcion("Producto lacteo");
+//            producto1.setPrecio(100.00);
+//            producto1.setTipo("POSTRE");
+            
+            listaProductos.add(producto1);
+            //listaProductos.add(producto2);
+            //listaProductos.add(producto3);
+            //listaProductos.add(producto4);
+            //listaProductos.add(producto5);
+
+            return listaProductos;
+        } catch (Exception ex) {
+            System.err.println("Error al cargar productos: " + ex.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    /**
+     * Obtiene la lista de ingredientes en el stock acutalizado.
+     * @return 
+     */
+    public List<IngredienteDTO> obtenerIngredientesStockActual() {
+        try {
+            return ingredienteBO.obtenerIngredienteTodos();
+        } catch (Exception ex) {
+            System.err.println("Error al cargar stock para vinculación: " + ex.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+
+    /**
+     * Obtiene la relacion de ProductoIngredientes, mockeado de momento al no poder manejar productos correctamente.
+     * @param producto
+     * @return 
+     */
+    public List<ProductoIngredienteRDTO> obtenerRecetaPorProducto(ProductoDTO producto) {
+        try {
+            
+            return obtenerRecetaPorProductoMock(producto);
+        } catch (Exception ex) {
+            System.err.println("Error al cargar la receta: " + ex.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    /**
+     * Guarda los datos de los nuevos ingredientes que agreguemos a un producto.
+     * @param producto
+     * @param nuevaReceta
+     * @throws Exception 
+     */
+    public void guardarRecetaDeProducto(ProductoDTO producto, List<ProductoIngredienteRDTO> nuevaReceta) throws Exception {
+        //cuando pueda manejar productos correctamente ya podré persistir los nuevos ingredientes que se quieran asociar aquí.
+        System.out.println("Guardando receta de " + nuevaReceta.size() + " ingredientes para: " + producto.getNombre());
+    }
+    
+    /**
+     * Metodo provisional que genera una receta.
+     * @param producto
+     * @return 
+     */
+    private List<ProductoIngredienteRDTO> obtenerRecetaPorProductoMock(ProductoDTO producto){
+            //Mock al no poder acoplar lista de productos en la BDD
+            List<ProductoIngredienteRDTO> listaRecetas = new ArrayList<>();
+            List<IngredienteDTO> listaIngredientes = ingredienteBO.obtenerIngredienteTodos();
+            
+            ProductoDTO producto1 = new ProductoDTO();
+            producto1.setId(1L);
+            producto1.setNombre("Ensalada");
+            producto1.setDescripcion("Verduras y topings");
+            producto1.setPrecio(100.00);
+            producto1.setTipo("COMIDA");
+            
+            ProductoIngredienteRDTO receta1 = new ProductoIngredienteRDTO();
+            receta1.setCantidad(5.0);
+            receta1.setProducto(producto1);
+            receta1.setIngrediente(listaIngredientes.get(0));
+            
+            ProductoIngredienteRDTO receta2 = new ProductoIngredienteRDTO();
+            receta2.setCantidad(2.0);
+            receta2.setProducto(producto1);
+            receta2.setIngrediente(listaIngredientes.get(1));
+            
+            listaRecetas.add(receta1);
+            listaRecetas.add(receta2);
+            
+            return listaRecetas;
+            
+            
     }
 }
