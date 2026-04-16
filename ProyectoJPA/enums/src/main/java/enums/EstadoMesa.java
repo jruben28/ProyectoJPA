2bd80ead19f3252c10337fc2ca0c311b45a62b52
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-04-16 08:11:37 -0400

    Cambié los enums a un modulo así como mis compañeros

---
+++ ProyectoJPA/enums/src/main/java/enums/EstadoMesa.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package enums;
+
+/**
+ * Estados posibles de una mesa del restaurante.
+ * @author Adrian Mendoza
+ */
+public enum EstadoMesa {
+    DISPONIBLE,
+    OCUPADA
+}
